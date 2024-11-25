794434f84622dbde011b026337b4a6984ba9bbe2
Alex <krisstalexx@gmail.com>
2024-11-25 17:40:44 +0100

    Added implemented functions to header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 18:30:15 by alisseye          #+#    #+#             */
-/*   Updated: 2024/11/25 17:11:35 by alisseye         ###   ########.fr       */
+/*   Updated: 2024/11/25 17:40:30 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@
 # include <stdio.h>
 
 t_list	*get_new_node(int content);
+void	push(t_list **dst, t_list **src);
+void	swap(t_list **stack);
+void	rotate(t_list **stack);
+void	reverse_rotate(t_list **stack);
 
 #endif
