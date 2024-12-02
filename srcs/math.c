14259d8f714adb509638eb0a87e1a81dbc09cb26
Alex <krisstalexx@gmail.com>
2024-12-02 20:32:04 +0100

    Operations + Operations interface, chunk_sort: chunk count determination

--- srcs/math.c
+++ srcs/math.c
@@ -1,39 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   math.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/25 17:10:41 by alisseye          #+#    #+#             */
-/*   Updated: 2024/12/02 16:17:21 by alisseye         ###   ########.fr       */
+/*   Created: 2024/12/02 20:08:04 by alisseye          #+#    #+#             */
+/*   Updated: 2024/12/02 20:09:30 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	free_stacks(t_list **a, t_list **b)
+int	ft_log2(double n)
 {
-	ft_lstclear(a, free);
-	free(a);
-	ft_lstclear(b, free);
-	free(b);
+	int	res;
+
+	res = 0;
+	while (n > 1)
+	{
+		n /= 2;
+		res++;
+	}
+	return (res);
 }
 
-t_list	*get_new_node(int content)
+int	ft_ceil(double n)
 {
-	t_list	*new_node;
+	int	res;
 
-	new_node = malloc(sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = malloc(sizeof(int));
-	if (!new_node->content)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	*(int *)new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
+	res = (int)n;
+	if (n > res)
+		res++;
+	return (res);
 }
--- srcs/operations/rrb.c
+++ srcs/operations/rrb.c
@@ -1,39 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   rrb.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/25 17:10:41 by alisseye          #+#    #+#             */
-/*   Updated: 2024/12/02 16:17:21 by alisseye         ###   ########.fr       */
+/*   Created: 2024/12/02 16:10:52 by alisseye          #+#    #+#             */
+/*   Updated: 2024/12/02 16:26:41 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	free_stacks(t_list **a, t_list **b)
+void	rrb(t_list **b)
 {
-	ft_lstclear(a, free);
-	free(a);
-	ft_lstclear(b, free);
-	free(b);
-}
-
-t_list	*get_new_node(int content)
-{
-	t_list	*new_node;
-
-	new_node = malloc(sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = malloc(sizeof(int));
-	if (!new_node->content)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	*(int *)new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
+	reverse_rotate(b);
+	ft_putstr_fd("rrb\n", 1);
 }
